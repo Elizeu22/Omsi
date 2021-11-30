cd57f13dea9bed0f13d2a1c7ce0e75ba7dbb8ab0
Elizeu22 <elizeu29.silva@gmail.com>
2021-11-30 15:10:01 -0300

    Omsi

---
+++ Omsi2/Omsi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Omsi2
+{
+    class Omsi
+    {
+        static void Main(string[] args)
+        {
+
+            string[] diretorio = Directory.GetFiles(@"Caminho do arquivo");
+     
+            foreach (string dir in diretorio)
+            {
+
+                if (dir.Contains("arquivo"))
+                {
+                  
+                    System.IO.File.Delete(dir);
+
+                }
+                else
+                {
+                    break;
+                }
+
+
+            }
+
+        }
+    }
+}
